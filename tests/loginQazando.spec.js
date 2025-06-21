bdd56e8382f0b86bea045d7f9d6d867164d2c9ae
JadsonCardoso <jadsonc190@gmail.com>
2025-06-21 13:11:29 -0300

    test: Testes  automatizados para tela de login

--- tests/loginQazando.spec.js
+++ tests/loginQazando.spec.js
@@ -1,5 +1,6 @@
 // @ts-check - spech
 import { test, expect } from '@playwright/test';
+import { log } from 'console';
 const {LoginPage} = require('./pages/loginPage')
 
 test('Acessar Aplicação', async ({ page }) => {
@@ -7,3 +8,42 @@ test('Acessar Aplicação', async ({ page }) => {
 
     await loginPage.acessarAplicação()
 }) 
+
+test('Login válido', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+
+    await loginPage.acessarAplicação()
+
+    await loginPage.preencherCampos('testes123@teste.com', '123456')
+    await loginPage.clicarBotaoLogin()
+
+    await loginPage.validarSucesso('Login realizado', 'Olá, testes123@teste.com')
+
+    await page.waitForTimeout(5000)
+})
+
+test('Login sem preencher campo Email', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+
+    await loginPage.acessarAplicação()
+
+    await loginPage.preencherCampos('', '123456')
+    await loginPage.clicarBotaoLogin()
+    
+    await loginPage.validarError('E-mail inválido.')
+
+    await page.waitForTimeout(5000)
+})
+
+test('Login sem preencher campo Senha', async ({ page }) => {
+    const loginPage = new LoginPage(page)
+
+    await loginPage.acessarAplicação()
+
+    await loginPage.preencherCampos('testes123@teste.com', '')
+    await loginPage.clicarBotaoLogin()
+    
+    await loginPage.validarError('Senha inválida.')
+
+    await page.waitForTimeout(5000)
+})
--- tests/pages/loginPage.js
+++ tests/pages/loginPage.js
@@ -4,8 +4,27 @@ export class LoginPage {
     constructor(page) {
         this.page = page
     }
+
 async acessarAplicação() {
     await this.page.goto('https://automationpratice.com.br/login');
     await expect(this.page.locator('#btnLogin')).toHaveText('login')
+}
+
+async preencherCampos(email, senha) {
+    await this.page.locator('#user').fill(email)
+    await this.page.locator('#password').fill(senha)
+}
+
+async clicarBotaoLogin() {
+    await this.page.locator('#btnLogin').click()
+}
+
+async validarError(mensagemError) {
+    await expect(this.page.locator('//span[@class="invalid_input"]')).toHaveText(mensagemError)
+}
+
+async validarSucesso(mensagemSucesso, validarEmail) {
+    await expect(this.page.locator('#swal2-title')).toHaveText(mensagemSucesso)
+    await expect(this.page.locator('#swal2-html-container')).toHaveText(validarEmail)
 }
 }
