615c484b3b6c022cdacb628fe86aa2b63dbf11b7
JadsonCardoso <jadsonc190@gmail.com>
2025-06-23 23:05:24 -0300

    feat: Pages: Components e Minha Conta

---
+++ tests/pages/Components.js
@@ -0,0 +1,2 @@
+
+/// Quando houver elemento com o mesmo HTML E CSS, coque nesse arquivo
---
+++ tests/pages/MinhaContaPage.js
@@ -0,0 +1,19 @@
+const { expect } = require('@playwright/test')
+
+export class MinhaContaPage {
+constructor(page) {
+        this.page = page
+    }
+
+      async validarCadastroSucesso(nome) {
+        await this.page.waitForLoadState('networkidle')
+        await expect(this.page.locator('#swal2-title')).toHaveText('Cadastro realizado!')
+        await expect(this.page.locator('#swal2-html-container')).toHaveText(`Bem-vindo ${nome}`)
+        await expect(this.page).toHaveURL(/.*my-account/)
+    }
+
+    async validarSucesso(mensagemSucesso, validarEmail) {
+    await expect(this.page.locator('#swal2-title')).toHaveText(mensagemSucesso)
+    await expect(this.page.locator('#swal2-html-container')).toHaveText(validarEmail)
+}
+}
