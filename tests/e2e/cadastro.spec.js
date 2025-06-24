24c7891a5630953ece965d965ce1fa488a3ffcac
JadsonCardoso <jadsonc190@gmail.com>
2025-06-23 23:00:53 -0300

    Test: Automatizando tela de Cadastro

---
+++ tests/e2e/cadastro.spec.js
@@ -0,0 +1,73 @@
+// @ts-check - spech
+
+const { test, expect } = require('@playwright/test') // Importando o modulo '@playwright/test' e expect, e passando para a função 'test'
+const { CadastroPage } = require('../pages/CadastroPage')
+const { MinhaContaPage } = require('../pages/MinhaContaPage')
+const { faker } = require('@faker-js/faker');
+
+let cadastroPage
+let minhaContaPage
+
+const userName = faker.person.fullName(); // Nome aleatório
+const userEmail = faker.internet.email(); // Email Aleatório
+
+test.beforeEach(async ({ page }) => {
+    cadastroPage = new CadastroPage(page)
+    minhaContaPage = new MinhaContaPage(page)
+})
+
+test('Cadastro com sucesso', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro(userName, userEmail, '123456')
+    await minhaContaPage.validarCadastroSucesso(userName)
+    await page.waitForTimeout(5000)
+
+})
+
+test('Não deve cadastrar sem preencher o formulário', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro('', '', '')
+    await cadastroPage.validarMensagemError('O campo nome deve ser prenchido')
+    await page.waitForTimeout(5000)
+
+})
+
+test('Não deve cadastrar se os campos email e senha não forem preenchidos', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro(userName, '', '')
+    await cadastroPage.validarMensagemError('O campo e-mail deve ser prenchido corretamente')
+    await page.waitForTimeout(5000)
+
+})
+
+test('Não deve cadastrar se os campos nome e senha não forem preenchidos', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro('', userEmail, '')
+    await cadastroPage.validarMensagemError('O campo nome deve ser prenchido')
+    await page.waitForTimeout(5000)
+
+})
+
+test('Não deve cadastrar se o campo senha não for preenchido', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro(userName, userEmail, '')
+    await cadastroPage.validarMensagemError('O campo senha deve ter pelo menos 6 dígitos')
+    await page.waitForTimeout(5000)
+
+})
+
+test('Não deve cadastrar com email inválido', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro(userName, 'wwww.dddd', '123456')
+    await cadastroPage.validarMensagemError('O campo e-mail deve ser prenchido corretamente')
+    await page.waitForTimeout(5000)
+
+})
+
+
+test('Não deve cadastrar se senha for menor que 6 caracteres', async ({ page }) => {
+    await cadastroPage.acessarCadastro()
+    await cadastroPage.submeterCadastro(userName, userEmail, '123')
+    await cadastroPage.validarMensagemError('O campo senha deve ter pelo menos 6 dígitos')
+    await page.waitForTimeout(5000)
+})
---
+++ tests/pages/CadastroPage.js
@@ -0,0 +1,30 @@
+const { expect } = require('@playwright/test')
+
+export class CadastroPage {
+
+    constructor(page) {
+        this.page = page
+    }
+
+    async acessarCadastro() {
+        await this.page.goto('https://automationpratice.com.br/register')
+
+        const cadastroForm = this.page.locator('.account_form')
+        await expect(cadastroForm).toBeVisible()
+    }
+
+    async submeterCadastro(nome, email, senha) {
+        await this.page.locator('#user').fill(nome)
+        await this.page.locator('#email').fill(email)
+        await this.page.locator('#password').fill(senha)
+        await this.page.locator('#btnRegister').click()
+    }
+
+    async clicarCadastrar() {
+        await this.page.locator('#btnRegister').click()
+    }
+
+    async validarMensagemError(texto) {
+        await expect(this.page.locator('.errorLabel')).toHaveText(texto)
+    }
+} 
